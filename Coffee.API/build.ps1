f9ab1f74e6dbd061f4eb00401ab6f0132cc8dd39
dtap001 <tapai.david.t@gmail.com>
2020-05-25 21:39:04 +0200

    Build fixes

--- Coffee.API/build.ps1
+++ Coffee.API/build.ps1
@@ -4,4 +4,5 @@ iex "ng build --prod=true"
 cd ..\Coffee.API\
 Write-Host "Building API"
 iex "npx tsc" 
-iex  "robocopy . Z:\DockerBuildDestination\coffee.API\. /IS /S /XD node_modules"
+#iex  "robocopy . Z:\DockerBuildDestination\coffee.API\. /IS /S /XD node_modules"
+iex  "robocopy . Z:\DockerBuildDestination\coffee.API\. /IS /S"
